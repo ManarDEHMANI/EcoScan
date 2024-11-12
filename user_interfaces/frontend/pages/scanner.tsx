24a6951a9af189f2f4df6cb6e7bd9ce7efd74dcb
ManarDEHMANI <manaar.dehmani@gmail.com>
2024-11-12 09:38:26 +0100

    update

---
+++ user_interfaces/frontend/pages/scanner.tsx
@@ -0,0 +1,13 @@
+// /pages/scanner.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Scanner = () => {
+  return (
+    <View>
+      <Text>Scanner Screen</Text>
+    </View>
+  );
+};
+
+export default Scanner;
